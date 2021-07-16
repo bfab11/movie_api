279e6a3e8ae5c77e577bc0af0153b96c3cc034d6
bfab11 <79426655+bfab11@users.noreply.github.com>
2021-07-16 13:39:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,20 +23,19 @@ app.use(bodyParser.json());
 
 let auth = require('./auth')(app);
 
-// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixdbapp.herokuapp.com'];
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
-//             let message = 'The CORS policy for this application does not allow access from the origin'
-//         origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixdbapp.herokuapp.com'];
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+            let message = 'The CORS policy for this application does not allow access from the origin'
+        origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
-app.use(cors());
 
 //get starting request
 app.get('/', (req, res) => {
